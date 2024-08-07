1a9714b056efd3e9e5e307b32f78d68d08e3304f
Sergey Izotov <sergey.izotov2005@gmail.com>
2024-08-07 21:26:54 +0500

    first commit

---
+++ src/main/java/com/example/main/controllers/MainController.java
@@ -0,0 +1,14 @@
+package com.example.main.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
